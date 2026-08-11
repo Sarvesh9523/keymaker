dbe45a597104f72d72f1b36b43fd83467b499bab
Sarvesh9523 <sarveshkumar9176@gmail.com>
2026-08-11 15:10:26 +0530

    frontend url is changed

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 // CORS configuration supporting cookies credentials
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: process.env.CLIENT_URL,
     credentials: true,
   })
 );
